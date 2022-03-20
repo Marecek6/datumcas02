8b458d86a3ff2d10098b60813e89832b8b81da94
Marecek6 <harudam.02@spst.eu>
2022-03-20 21:51:12 +0100

    Přidejte soubory projektu.

---
+++ datumcas02/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datumcas02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static TimeSpan pocetDnu(DateTime narozen)
+        {
+            TimeSpan uplynulo;
+            narozen = narozen.AddYears(65);
+            uplynulo = DateTime.Today - narozen;
+            return uplynulo;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("pocet dnu od doby, kdy osoba dovršila 65 let " + pocetDnu(dateTimePicker1.Value).Days);
+        }
+    }
+}
